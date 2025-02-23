b10fcce1bc4dff1a169cbb89f81e39264ec0ba45
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-23 15:41:04 +0500

    Update Task 2.c

--- LAB 4/Task 2.c
+++ LAB 4/Task 2.c
@@ -1,1 +1,28 @@
-
+#include<stdio.h>
+int main()
+{
+	int n,flag=1;
+	printf("enter a number: ");
+	scanf("%d",&n);
+	if(n==1){
+		printf("number is not prime\n");
+		return 0;
+	}
+	else{
+	
+	int i;
+	for ( i=2; i<=n-1;i++){
+		if(n%i==0){
+			flag=0;
+			break;
+	}
+	}
+	}
+	if (flag==1){
+		printf("number is prime");
+	}
+	else{
+			printf("number is not prime");
+	}
+	return 0;
+}
