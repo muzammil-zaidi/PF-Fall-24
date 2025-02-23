3fe3951e1c2fb7e4668e7b94b463e88594d78093
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-23 15:40:32 +0500

    Update Task 1.c

--- LAB 4/Task 1.c
+++ LAB 4/Task 1.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+	int sum,n;
+	while(n!=0){
+		printf("enter a number: ");
+		scanf("%d",&n);
+		sum=sum+n;
+		printf("number is %d and the sum is %d\n",n,sum);
+	}
+	return 0;	
+}
