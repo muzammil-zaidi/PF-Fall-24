364e81c54cc28d8896ff927da23b106473e299d4
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-24 20:35:55 +0500

    Update Task 5.c

--- LAB 4/Task 5.c
+++ LAB 4/Task 5.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=1;i<35;i++){
+		if(i%6==0){
+			printf("\n");
+		}
+		if(i<6 || i>30 || i%6==0 || (i+1)%6==5){
+			printf("* ");
+		}
+		else{
+			printf("  ");
+		}
+	}
+	return 0;
+}
