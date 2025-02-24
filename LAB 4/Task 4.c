5cde25a3d740c73a9773a001dac5d93e4f12c8a9
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-24 20:35:10 +0500

    Update Task 4.c

--- LAB 4/Task 4.c
+++ LAB 4/Task 4.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,num,r,z=0;
+	printf("enter a number: ");
+	scanf("%d",&n);
+	num=n;
+	while(num!=0){
+		r=num%10;
+		z=z+pow(r,3);
+		num=num/10;
+	}
+	if(z==n){
+		printf("%d number is an armstrong number",n);
+	}
+	else{
+		printf("%d number is not an armstrong number",n);
+	}
+	return 0;
+}
