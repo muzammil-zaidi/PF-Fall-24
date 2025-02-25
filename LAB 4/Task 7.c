4242fb0bc872b5e22963444f36984b88a6c4f0c8
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-25 17:53:48 +0500

    Update Task 7.c

--- LAB 4/Task 7.c
+++ LAB 4/Task 7.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+int main() 
+{
+    char x='A'; 
+	int i;
+    for(i=0;i<5;i++){
+        printf("%c ",x+i);
+    }
+    printf("\n");
+	for(i=1;i<=4;i++){
+        printf("%c",x+i);
+        printf("       ");
+        printf("%c\n",x+i+4);
+    }
+    for(i=5;i<10;i++){
+        printf("%c ",x+i);
+    }
+    return 0;
+}
