0fef592ea3c4ab59d777c32137f26bdee04b20f0
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-25 17:53:18 +0500

    Update Task 6.c

--- LAB 4/Task 6.c
+++ LAB 4/Task 6.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main() 
+{
+    printf("*       *\n");
+	int x=1;
+    do {
+        printf("* * * * * \n");
+        x++;
+    } while (x<5); 
+	printf("*       *\n");
+	return 0;
+}
