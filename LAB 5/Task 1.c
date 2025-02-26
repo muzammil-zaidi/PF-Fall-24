9a49eaa3f6e4599007e8c9b99f052cec309df04d
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-26 23:38:10 +0500

    Update Task 1.c

--- LAB 5/Task 1.c
+++ LAB 5/Task 1.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter number of elements: ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter %d elements:\n",n);
+	int sum=0;
+	int i;
+	for(i=0;i<n;i++){
+		printf("enter number: ");
+		scanf("%d",&arr[i]);
+		sum=sum+arr[i];
+	}
+	printf("sum of elements is %d\n",sum);
+	return 0;
+}
