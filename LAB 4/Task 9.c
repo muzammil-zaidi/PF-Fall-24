9d484e56a68e157a2079fea839b898477769808e
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-26 23:35:48 +0500

    Update Task 9.c

--- LAB 4/Task 9.c
+++ LAB 4/Task 9.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+int main() 
+{
+    int i;
+	for (i=0;i<5;i++){
+        if(i==0){
+            printf("* * * * *\n");
+        }
+        else if(i==1){
+            printf("  *   *   \n");
+        }
+        else if(i==2){
+            printf("    *    \n");
+        }
+        else if(i==3){
+            printf("  *   *  \n");
+        }
+        else if(i==4){
+            printf("* * * * *");
+        }
+    }
+    return 0;
+}
