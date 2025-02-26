00f5fbc034796c6c044e690fc6939dda7467e740
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-26 23:34:57 +0500

    Update Task 8.c

--- LAB 4/Task 8.c
+++ LAB 4/Task 8.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+int main() 
+{
+    int i;
+	for (i=0;i<5;i++){
+        if(i==0){
+            printf("A B C D E\n");
+        }
+        else if(i==1){
+            printf("  C   F  \n");
+        }
+        else if(i==2){
+            printf("    E    \n");
+        }
+        else if(i==3){
+            printf("  E   H  \n");
+        }
+        else if(i==4){
+            printf("F G H I J");
+        }
+    }
+    return 0;
+}
