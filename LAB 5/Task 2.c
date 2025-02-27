9e324cd76f376d9ada9d9f40a57ea737b9221f0f
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-27 23:46:24 +0500

    Update Task 2.c

--- LAB 5/Task 2.c
+++ LAB 5/Task 2.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+int main()
+{
+	int n;
+ 	printf("enter the number of elements: ");
+ 	scanf("%d",&n);
+ 	int arr[n];
+ 	printf("enter %d elements:\n", n);
+ 	int i;
+	for(i=0;i<n;i++){
+		printf("enter number: ");
+ 		scanf("%d", &arr[i]);
+ 	}
+ 	printf("reverse order is: ");
+	for (i=n-1;i>=0;i--){
+ 		printf("%d ", arr[i]);
+ 	}
+ 	return 0;
+}
