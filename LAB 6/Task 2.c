7ac581cbfb3b42db5b69cb40dc8ae451b4c00648
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-28 17:42:46 +0500

    Update Task 2.c

--- LAB 6/Task 2.c
+++ LAB 6/Task 2.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
 
+int main(){
+    int n,i,count=1;
+    printf("enter an odd number to start the pattern: ");
+    scanf("%d",&n);
+    
+    if(n%2==0){
+        printf("please enter an odd number\n");
+        return 0;
+    }
+    while(n>=1){
+        for(i=0;i<count && n>=1;i++){
+            printf("%2d  ",n);
+            n=n-2;
+        }
+        printf("\n");
+        count++;
+	}
+    return 0;
+}
