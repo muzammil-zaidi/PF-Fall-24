7f1e942870e102f4b1be60dbc5be77022f8b5b08
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-28 17:40:19 +0500

    Update Task 4.c

--- LAB 5/Task 4.c
+++ LAB 5/Task 4.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
 
+int main(){
+	int n;
+	printf("enter the number of elements: ");
+	scanf("%d",&n);
+	int arr[n];
+	int count[n];
+	int found=0;
+	printf("enter %d elements:\n",n);
+	int i;
+	for(i=0;i<n;i++){
+		count[i]=0;
+	}
+	for(i=0;i<n;i++){
+		printf("enter number: ");
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++){
+		count[arr[i]]++;
+	}
+	printf("elements occuring more than once: ");
+	for(i=0;i<n;i++){
+		if(count[i]>1){
+			printf("%d ",i);
+			found=1;
+		}
+	}
+	if(!found){
+		printf("none");
+	}
+	return 0;
+}
