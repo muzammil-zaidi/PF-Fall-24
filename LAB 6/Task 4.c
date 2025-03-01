f7c547dc2991cb1ddd4c6645d329156f0b9f7ad7
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-01 19:22:04 +0500

    Update Task 4.c

--- LAB 6/Task 4.c
+++ LAB 6/Task 4.c
@@ -1,1 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int m1[3][3],m2[3][3],result[3][3],i,j,k;
 
+    printf("Enter elements of the first 3x3 matrix:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&m1[i][j]);
+        }
+    }
+    
+    printf("Enter elements of the second 3x3 matrix:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&m2[i][j]);
+        }
+    }
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            result[i][j]=0;
+        }
+    }
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            for(k=0;k<3;k++){
+                result[i][j]+=m1[i][k]*m2[k][j];
+            }
+        }
+    }
+
+    printf("Result after multiplication:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%3d  ",result[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
