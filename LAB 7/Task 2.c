b4d40ec836218e5054e4f7d595e9336bc94a27e3
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-03 22:52:24 +0500

    Update Task 2.c

--- LAB 7/Task 2.c
+++ LAB 7/Task 2.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+void swap(int n1,int n2){
+	int temp=n1;
+	n1=n2;
+	n2=temp;
+	printf("A=%d , B=%d",n1,n2);
+}
+
+int main()
+{
+	int n1,n2;
+	printf("Enter A = ");
+	scanf("%d",&n1);
+	printf("Enter B = ");
+	scanf("%d",&n2);
+	swap(n1,n2);
+	return 0;
+}
