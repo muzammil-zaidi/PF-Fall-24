33426e7a038d6d8bcbaccf8cff0858a509aeb5dc
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-03 22:30:05 +0500

    Update Task 5.c

--- LAB 6/Task 5.c
+++ LAB 6/Task 5.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int x,i,j;
+    printf("Enter the number of rows for the upper half of the diamond: ");
+    scanf("%d",&x);
+    
+    for(i=1;i<=x;i++){
+        for(j=i;j<x;j++){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
 
+    for(i=x-1;i>=1;i--){
+        for(j=x;j>i;j--){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
