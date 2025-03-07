4566a3405c064c23afcdebc985ad9521ec02ca64
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-08 00:09:54 +0500

    Update Task 3.c

--- LAB 7/Task 3.c
+++ LAB 7/Task 3.c
@@ -1,1 +1,32 @@
-
+#include<stdio.h>
+void prime(int n){
+	int i,prime;
+	if(n>1){
+		for(i=2;i<n-1;i++){
+			if(n%i==0){
+				prime=0;
+				break;
+			}
+			else{
+				prime=1;
+			}
+		}
+			if(prime){
+		printf("%d is prime number",n);
+	}
+	else{
+		printf("%d is not prime number",n);
+	}
+	}
+	else{
+		printf("%d is not prime number",n);
+	}
+}
+int main()
+{
+	int n;
+	printf("Enter a number: ");
+	scanf("%d",&n);
+	prime(n);
+	return 0;
+}
