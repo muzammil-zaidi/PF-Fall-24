8425a0888415f1cea64a0b7e6e70c6faa0c417bc
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-13 00:09:00 +0500

    Update Task 4.c

--- LAB 7/Task 4.c
+++ LAB 7/Task 4.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
 
+void add(int num1, int num2) {
+    printf("%d + %d = %d\n", num1, num2, num1 + num2);
+}
+
+void subtract(int num1, int num2) {
+    printf("%d - %d = %d\n", num1, num2, num1 - num2);
+}
+
+void multiply(int num1, int num2) {
+    printf("%d * %d = %d\n", num1, num2, num1 * num2);
+}
+
+void divide(int num1, int num2) {
+    if (num2 != 0) {
+        printf("%d / %d = %.2f\n", num1, num2, (float)num1 / num2);
+    } else {
+        printf("Error: Division by zero is not allowed.\n");
+    }
+}
+
+int main() {
+    int num1, num2;
+    char operation;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    printf("Enter operation (+, -, *, /): ");
+    scanf(" %c", &operation); // Space before %c to consume any leftover whitespace
+
+    switch (operation) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+        default:
+            printf("Error: Invalid operation.\n");
+            break;
+    }
+
+    return 0;
+}
