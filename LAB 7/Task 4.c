08a9b41f7c814314e04d4b3fe7f129a799d6f3a7
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-14 00:11:05 +0500

    Update Task 4.c

--- LAB 7/Task 4.c
+++ LAB 7/Task 4.c
@@ -13,9 +13,9 @@ void multiply(int num1, int num2){
 }
 
 void divide(int num1, int num2){
-    if (num2 != 0) {
+    if (num2 != 0){
         printf("%d / %d = %.2f\n", num1, num2, (float)num1 / num2);
-    } else {
+    } else{
         printf("Error: Division by zero is not allowed.\n");
     }
 }
