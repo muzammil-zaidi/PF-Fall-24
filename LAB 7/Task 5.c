388d38afafd8c1d4e77a784337f4965fc02e3f30
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-15 22:26:06 +0500

    Update Task 5.c

--- LAB 7/Task 5.c
+++ LAB 7/Task 5.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+#include<string.h>
+void reverse(char str[]){
+    int n=strlen(str);
+    int i;
+	for(i=0;i<n/2;i++){
+        char temp=str[i];
+        str[i] = str[n-i-1];
+        str[n-i-1]=temp;
+    } 
+    printf("Reversed string: %s\n",str); 
+}
+int main()
+{
+    char str[]="Hello World"; 
+	reverse(str); 
+    return 0;
+}
