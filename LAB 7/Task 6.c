e294496fda50a6c8b5538f4b8bff54cf1671a602
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-16 23:54:43 +0500

    Update Task 6.c

--- LAB 7/Task 6.c
+++ LAB 7/Task 6.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int max,min,i;
+void MaxMin(int arr[]){
+    max=arr[0]; 
+    min=arr[0]; 
+	for (i=1;i<5;i++){
+        if(arr[i]>max){
+            max=arr[i]; 
+        }
+        if(arr[i]<min){
+            min=arr[i]; 
+        }
+    }
+}
+int main()
+{
+    int arr[5]={6,7,3,9,5}; 
+	MaxMin(arr);
+	printf("Maximum number = %d\n",max);
+    printf("Minimum number = %d\n",min);
+    return 0;
+}
