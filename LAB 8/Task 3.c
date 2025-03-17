9297a1fb539a6363aadbbab2dd5b1cc65095ff73
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-18 00:53:13 +0500

    Update Task 3.c

--- LAB 8/Task 3.c
+++ LAB 8/Task 3.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+int power(int a,int b){
+    if(b==0){
+        return 1;
+    }
+    return a*power(a,b-1);
+}
 
+int main(){
+    int a,b,c;
+    printf("Enter base (a): ");
+    scanf("%d",&a);
+    printf("Enter exponent (b): ");
+    scanf("%d",&b);
+    c=power(a,b);
+    printf("%d raised to the power of %d is %d\n",a,b,c);
+    return 0;
+}
