6f3a4952a88c4835fb6fd9c8dd5d2308b6ccd1d6
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-03-19 01:25:48 +0500

    Update Task 4.c

--- LAB 8/Task 4.c
+++ LAB 8/Task 4.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+int count(int n,int t){
+    if(n==0){
+        return 0;
+    }
+    if (n%10==t){
+        return 1+count(n/10,t); 
+    }
+    else{
+        return count(n/10,t);
+    }
+}
 
+int main() {
+    int n,t,output;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    printf("Enter target digit: ");
+    scanf("%d",&t);
+    output=count(n,t);
+    printf("The digit %d appears %d times in the number %d.\n",t, output,n);
+    return 0;
+}
