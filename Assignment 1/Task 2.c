80e9bdfa40b2cb81c93e6f187fd9a36cd23eea39
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-04-04 01:20:53 +0500

    Update Task 2.c

--- Assignment 1/Task 2.c
+++ Assignment 1/Task 2.c
@@ -1,1 +1,33 @@
-
+#include<stdio.h>
+#include<string.h>
+int main(){
+	int i,j,a,b=0,found=0,count[26];
+	char letters[26],string[30];
+	printf("Enter slogan:");
+	gets(string);
+	a=strlen(string);
+	for(i=0;i<26;i++){
+		count[i]=0;
+		letters[i]=0;	
+	}
+	for(i=0;i<a;i++){
+		for(j=0;j<26;j++){
+			if(string[i]==letters[j]){
+				count[j]=count[j]+1;
+				found=1;
+				break;
+			}
+		}
+		if(found==0){
+			letters[b]=string[i];
+			count[b]=count[b]+1;
+			b=b+1;
+		}
+	}
+	for(i=0;i<26;i++){
+	//	if(count[i]>0){
+	//		printf("'%c':%d, ",letters[i],count[i]);
+	//	}
+	}
+	return 0;
+}
