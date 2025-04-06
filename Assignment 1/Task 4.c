80c21a6086a536c34cdfa7d30729b6f985f65ca0
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-04-06 14:38:16 +0500

    Update Task 4.c

--- Assignment 1/Task 4.c
+++ Assignment 1/Task 4.c
@@ -1,1 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+void letters(char word[]){
+    int length=strlen(word),i,j;  
+    char temp;
+    for(i=0;i<length-1;i++){  
+        for(j=0;j<length-i-1;j++){  
+            if(word[j]>word[j+1]){  
+                temp=word[j];
+                word[j]=word[j+1];
+                word[j+1]=temp;
+            }
+        }
+    }
+}
 
+int main(){
+    int a,i,j;
+    printf("Enter the number of words: ");
+    scanf("%d",&a);  
+    char str_arr[a][30],sort_arr[a][30];  
+    int flag[a];
+    memset(flag,0,sizeof(flag));
+    printf("Enter the words:\n");
+    for(i=0;i<a;i++){
+        scanf("%s",str_arr[i]);  
+        strcpy(sort_arr[i],str_arr[i]); 
+        letters(sort_arr[i]);
+    }
+    printf("\nOutput:\n");
+    for(i=0;i<a;i++){
+        if(flag[i])
+		continue;
+        printf(" {'%s'",str_arr[i]);
+        flag[i]=1;
+        for(j=i+1;j<a;j++){
+            if(!flag[j] && strcmp(sort_arr[i],sort_arr[j])==0){
+                printf(" '%s'",str_arr[j]);
+                flag[j]=1;
+            }
+        }
+        printf(" }\n");
+    }
+    return 0;
+}
