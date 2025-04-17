ffaf4b5d1376e0cc88c4b2171f028760bb695cee
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-04-17 19:18:35 +0500

    Update Task 6.c

--- Assignment 1/Task 6.c
+++ Assignment 1/Task 6.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+int matchstick(int n){
+    if(n%5==0){
+        return 0;
+    }
+	else{
+        return n%5;
+    }
+}
 
+int main(){
+    int n;
+    printf("Enter the number of matchsticks: ");
+    scanf("%d",&n);
+    int result=matchstick(n);
+    if (result==0){
+        printf("It is impossible to win for A\n");
+    } else {
+        printf("A should pick %d matchsticks to guaranteed a win.\n",result);
+    }
+    return 0;
+}
