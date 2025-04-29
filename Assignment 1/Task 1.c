67a4eac5405d8944938c436416bdc4e7a5bbdece
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-04-29 17:31:11 +0500

    Update Task 1.c

--- Assignment 1/Task 1.c
+++ Assignment 1/Task 1.c
@@ -9,24 +9,30 @@ int main(){
         printf("Element-%d: ",i);
         scanf("%d",&arr[i]);
     }
+    
     int index=0;
     min=arr[0];
+    
     for(i=0;i<5;i++){
         if(arr[i]<min){
             min=arr[i];
             index=i;
         }
     }
+    
     for(i=0;i<5;i++){
         if(arr[i]==min){
             arr[i]=INT_MAX;
         }
     }
+    
     s_min=arr[index];
+    
     for(i=0;i<5;i++){
         if(arr[i]<s_min){
             s_min=arr[i];
         }
     }
+    
     printf("The second smallest element in the array is %d",s_min);
 }
