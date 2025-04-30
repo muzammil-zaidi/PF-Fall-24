f1f1501ab164ed0a2480b681061e879332b74d50
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-05-01 01:03:58 +0500

    Update Task 3.c

--- Assignment 2/Task 3.c
+++ Assignment 2/Task 3.c
@@ -1,1 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+int validateEmail(char* email){
+    if (email==NULL || strlen(email)==0){
+        return 0;
+    }
+    int i,count=0;
+    for(i=0;i<strlen(email);i++){
+        if(email[i]=='@'){
+            count++;
+        }
+    }
+    if(count!=1){
+        return 0;
+    }
+    char* atPosition=strchr(email,'@');
+    if (atPosition==NULL || strchr(atPosition,'.')==NULL){
+        return 0;
+    }
+    return 1;
+}
+
+int main(){
+    char* email=NULL;
+    size_t size=0;
+    printf("Enter an Email Address: ");
+    email=(char*)malloc(100*sizeof(char));
+    if(email==NULL){
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    fgets(email,100,stdin);
+    email[strcspn(email,"\n")]=0;
+    if(validateEmail(email)){
+        printf("Valid Email\n");
+    }
+	else{
+        printf("Invalid Email\n");
+    }
+    free(email);
+    return 0;
+}
